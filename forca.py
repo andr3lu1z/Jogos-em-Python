615fb17a8ad696facf0426fcd49b3b32a4947cf6
Andre Almeida <andrealmeida.dev@gmail.com>
2021-11-19 14:05:23 -0300

    atualizando

--- forca.py
+++ forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("***Bem-vindo ao Jogo da Forca!**")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+        print("Jogando...")
+
     print("FIM DE JOGO")
 
 if(__name__ == "__main__"):
