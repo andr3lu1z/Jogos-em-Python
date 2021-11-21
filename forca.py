8f8e1d9eb8b876f5b643d39c824a1ecb30d90981
Andre Almeida <andrealmeida.dev@gmail.com>
2021-11-21 10:53:52 -0300

    Atualizando o jogo da forca

--- forca.py
+++ forca.py
@@ -1,26 +1,52 @@
+import random
+
 def jogar():
     print("********************************")
     print("***Bem-vindo ao Jogo da Forca!**")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    tentativa = 0
 
-    while (not acertou and not enforcou):
-        chute = input("Qual letra? ")
-        chute = chute.strip()
+    print(letras_acertadas)
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativa += 1
+
+        enforcou = tentativa == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if (acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("FIM DE JOGO")
 
 if(__name__ == "__main__"):
